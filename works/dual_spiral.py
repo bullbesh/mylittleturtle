cf9a69cfb3e124fb8ba70d06f3cc68c6a3b0e61c
bullbesh <babulehaha@gmail.com>
2022-04-19 17:16:20 +0300

    Change the case of values to uppercase

--- works/dual_spiral.py
+++ works/dual_spiral.py
@@ -4,15 +4,15 @@ import time
 
 t = turtle.Turtle()
 turtle.title("Dual Spiral")
-turtle.bgcolor("beige")
+turtle.bgcolor("Beige")
 t.speed(1000)
 
 
 def create_spiral():
     for i in range(120):
-        t.color("red")
+        t.color("Red")
         t.circle(i)
-        t.color("orange")
+        t.color("Orange")
         t.circle(i * 0.8)
         t.left(3)
         t.backward(3)
